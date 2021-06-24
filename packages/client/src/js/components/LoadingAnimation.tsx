f721ce9d8f889dfbfb84cddf1016d80c6eaedb31
Tracer1337 <merlin.moelter@gmail.com>
2021-06-24 19:45:06 +0200

    Remove loading text from loading animation

--- packages/client/src/js/components/LoadingAnimation.tsx
+++ packages/client/src/js/components/LoadingAnimation.tsx
@@ -3,7 +3,6 @@ import React from "react"
 export default function LoadingAnimation() {
     return (
         <div className="loading-animation">
-            <h2 className="text">Loading...</h2>
             <img src="assets/images/loading-animation.gif" alt="Loading" />
         </div>
     )
