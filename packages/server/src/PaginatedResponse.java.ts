4c249b965d0b9f2bbe421016920e841de0d0a21f
Tracer1337 <merlin.moelter@gmail.com>
2021-06-27 22:50:19 +0200

    Add pagination to search api

---
+++ packages/server/src/PaginatedResponse.java.ts
@@ -0,0 +1,36 @@
+export default class PaginatedResponse<T> {
+    private items: T[]
+    private page: number
+    private itemsPerPage: number
+    
+    constructor(values: {
+        items: T[],
+        page: number,
+        itemsPerPage: number
+    }) {
+        this.items = values.items
+        this.page = values.page
+        this.itemsPerPage = values.itemsPerPage
+    }
+
+    private getAmountOfPages() {
+        return Math.ceil(this.items.length / this.itemsPerPage)
+    }
+
+    private paginate() {
+        return this.items.slice(
+            this.itemsPerPage * this.page,
+            this.itemsPerPage * (this.page + 1)
+        )
+    }
+
+    toJSON() {
+        return {
+            meta: {
+                currentPage: this.page + 1,
+                totalPages: this.getAmountOfPages()
+            },
+            items: this.paginate()
+        }
+    }
+}
--- packages/server/src/Server.java.ts
+++ packages/server/src/Server.java.ts
@@ -4,9 +4,11 @@ import Search from "search"
 import path from "path"
 import dotenv from "dotenv"
 dotenv.config({ path: path.join(__dirname, "..", ".env") })
+import PaginatedResponse from "./PaginatedResponse.java"
 
 export default class Server {
     private static readonly PORT = process.env.PORT
+    private static readonly ITEMS_PER_PAGE = 20
 
     public static async main(args: string[]) {
         const server = new Server()
@@ -29,14 +31,27 @@ export default class Server {
 
     private async handleSearch(req: Request, res: Response) {
         const query = req.query.q as string
-        if (!query || typeof query !== "string") {
+        let page = parseInt(req.query.page as any)
+        page = Number.isFinite(page) && page > 0 ? page : 1
+        page -= 1
+
+        if (query.length === 0) {
             return res.send([])
         }
-        const keywords = query.split(" ")
+
+        const keywords = query
+            .split(" ")
+            .map((word) => encodeURIComponent(word))
+
         try {
             const search = await Search.createInstance(keywords)
             const result = await search.getSearchResults()
-            return res.send(result)
+            const response = new PaginatedResponse({
+                items: result,
+                page,
+                itemsPerPage: Server.ITEMS_PER_PAGE
+            })
+            return res.send(response)
         } catch (error) {
             console.error(error)
             return res.sendStatus(500)
